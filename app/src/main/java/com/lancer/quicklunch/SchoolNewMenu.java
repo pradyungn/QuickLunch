9095afaf954e996d403dc9f8fb679062c86b804a
ethanshedd <eshedd20@dtechhs.org>
2019-02-09 15:33:51 -0800

    Publish Menu Added and Linked

--- app/src/main/java/com/lancer/quicklunch/SchoolNewMenu.java
+++ app/src/main/java/com/lancer/quicklunch/SchoolNewMenu.java
@@ -1,7 +1,10 @@
 package com.lancer.quicklunch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SchoolNewMenu extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SchoolNewMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_new_menu);
+        Button btn = (Button)findViewById(R.id.button3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SchoolNewMenu.this, SchoolLiveMenu.class));
+            }
+        });
     }
 }
