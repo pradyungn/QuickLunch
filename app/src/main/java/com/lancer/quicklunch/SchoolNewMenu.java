1e912be3b92f5cdcf05a84e4412b4ab968ae5e6f
ethanshedd <eshedd20@dtechhs.org>
2019-02-09 12:32:15 -0800

    Activities and Logo

---
+++ app/src/main/java/com/lancer/quicklunch/SchoolNewMenu.java
@@ -0,0 +1,13 @@
+package com.lancer.quicklunch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SchoolNewMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school_new_menu);
+    }
+}
---
+++ app/src/main/java/com/lancer/quicklunch/StudentCheckout.java
@@ -0,0 +1,13 @@
+package com.lancer.quicklunch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentCheckout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_checkout);
+    }
+}
