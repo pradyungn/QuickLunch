2c6d3454876eb7bc7d01cc104b4fa98f7ca33431
ethanshedd <eshedd20@dtechhs.org>
2019-02-09 19:17:53 -0800

    HARD CODE TIME

--- app/src/main/java/com/lancer/quicklunch/SchoolNewMenu.java
+++ app/src/main/java/com/lancer/quicklunch/SchoolNewMenu.java
@@ -14,27 +14,16 @@ public class SchoolNewMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_school_new_menu);
-        Button btn = (Button)findViewById(R.id.publish);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button btn1 = (Button)findViewById(R.id.newRow);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SchoolNewMenu.this, SchoolLiveMenu.class));
+                startActivity(new Intent(SchoolNewMenu.this, SchoolNewMenuDos.class));
             }
         });
-        /* Find Tablelayout defined in main.xml */
-        TableLayout tl = (TableLayout) findViewById(R.id.tableLayout1);
-        /* Create a new row to be added. */
-        TableRow tr = new TableRow(this);
-        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-        /* Create a Button to be the row-content. */
-        Button b = (Button)findViewById(R.id.newRow);
-        b.setText("Dynamic Button");
-        b.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-        /* Add Button to row. */
-        tr.addView(b);
-        /* Add row to TableLayout. */
-//tr.setBackgroundResource(R.drawable.sf_gradient_03);
-        tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+
+
     }
 }
