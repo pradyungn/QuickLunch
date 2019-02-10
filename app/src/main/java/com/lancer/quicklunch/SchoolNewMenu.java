89d077818fef9fc775502d6900ad1342ada43a25
ethanshedd <eshedd20@dtechhs.org>
2019-02-09 18:35:44 -0800

    Setting up table adding

--- app/src/main/java/com/lancer/quicklunch/SchoolNewMenu.java
+++ app/src/main/java/com/lancer/quicklunch/SchoolNewMenu.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class SchoolNewMenu extends AppCompatActivity {
 
@@ -20,5 +22,19 @@ public class SchoolNewMenu extends AppCompatActivity {
                 startActivity(new Intent(SchoolNewMenu.this, SchoolLiveMenu.class));
             }
         });
+        /* Find Tablelayout defined in main.xml */
+        TableLayout tl = (TableLayout) findViewById(R.id.tableLayout1);
+        /* Create a new row to be added. */
+        TableRow tr = new TableRow(this);
+        tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        /* Create a Button to be the row-content. */
+        Button b = new Button(this);
+        b.setText("Dynamic Button");
+        b.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        /* Add Button to row. */
+        tr.addView(b);
+        /* Add row to TableLayout. */
+//tr.setBackgroundResource(R.drawable.sf_gradient_03);
+        tl.addView(tr, new TableLayout.LayoutParams(TableLayout.LayoutParams.FILL_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
     }
 }
